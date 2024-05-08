cdeee03695ed7f5d96a6a2f4ad24f681b677d162
Chisholm6192 <rmwc92@gmail.com>
2024-05-08 17:23:56 -0400

    Encryption Functions

---
+++ substitution.java
@@ -0,0 +1,31 @@
+public class substitution {
+
+    final static String key = "]kYV}(!7P$n5_0i R:?jOWtF/=-pe'AD&@r6%ZXs\"v*N"
+            + "[#wSl9zq2^+g;LoB`aGh{3.HIu4fbK)mU8|dMET><,Qc\\C1yxJ";
+
+    static String text = "This is a substitution cipher method of encryption.";
+
+    public static void main(String[] args) {
+        String enc = encode(text);
+        System.out.println("Encoded: " + enc);
+        System.out.println("\nDecoded: " + decode(enc));
+    }
+
+    static String encode(String s) {
+        StringBuilder sb = new StringBuilder(s.length());
+
+        for (char c : s.toCharArray())
+            sb.append(key.charAt((int) c - 32));
+
+        return sb.toString();
+    }
+
+    static String decode(String s) {
+        StringBuilder sb = new StringBuilder(s.length());
+
+        for (char c : s.toCharArray())
+            sb.append((char) (key.indexOf((int) c) + 32));
+
+        return sb.toString();
+    }
+}
